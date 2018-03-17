059e8eab38209f59955f541b84701b8f51b7696a
deepak.s <deepak.s@neoito.com>
2018-03-17 14:34:03 +0530

    hover on  button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TextFieldComponent } from './text-field/text-field.component';
 import { AnimatedBellComponent } from './animated-bell/animated-bell.component';
+import { ButtonComponent } from './button/button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TextFieldComponent,
-    AnimatedBellComponent
+    AnimatedBellComponent,
+    ButtonComponent
+
   ],
   imports: [
     BrowserModule
