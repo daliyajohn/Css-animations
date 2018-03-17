cc4e89f2de7f2b06c0bd1c35a06a68a11b9e51c6
daliyajohn <daliyajohn75@gmail.com>
2018-03-17 14:57:25 +0530

    Listing animation fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TextFieldComponent } from './text-field/text-field.component';
 import { AnimatedBellComponent } from './animated-bell/animated-bell.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TextFieldComponent,
-    AnimatedBellComponent
+    AnimatedBellComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
