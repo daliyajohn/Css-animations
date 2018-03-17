31ee09f2e9a05a4b3b8c9245f01aff0b3be0dc59
daliyajohn <daliyajohn75@gmail.com>
2018-03-17 12:20:00 +0530

    Add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TextFieldComponent } from './text-field/text-field.component';
+import { AnimatedBellComponent } from './animated-bell/animated-bell.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextFieldComponent
+    TextFieldComponent,
+    AnimatedBellComponent
   ],
   imports: [
     BrowserModule
