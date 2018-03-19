95c66078e2ad639f4efb8ca60567f439fbd7d1f5
safal <safaldasg@gmail.com>
2018-03-19 18:06:51 +0530

    unsubscribe svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,27 @@ import { Button1Component } from './button1/button1.component';
 import { AnimatedBellComponent } from './animated-bell/animated-bell.component';
 import { FormComponent } from './form/form.component';
 import { ButtonComponent } from './button/button.component';
+import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    TextFieldComponent,
-    ButtonComponent,
-    Button1Component,
-  ],
-  imports: [
-    BrowserModule,
-    AnimatedBellComponent,
-    FormComponent,
-    ButtonComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule(
+  {
+    declarations: [
+      AppComponent,
+      TextFieldComponent,
+      ButtonComponent,
+      Button1Component,
+      UnsubscribeComponent
+    ],
+    imports: [
+      BrowserModule,
+      AnimatedBellComponent,
+      FormComponent,
+      ButtonComponent
+    ],
+    providers: [],
+    bootstrap: [
+      AppComponent
+    ]
+  }
+)
 export class AppModule {}
